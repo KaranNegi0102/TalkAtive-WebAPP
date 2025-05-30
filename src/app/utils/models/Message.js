13c2adf6d62f22aa434e4f3d74abd5ef0dcb3919
KaranNegi0102 <karannegi0102@gmail.com>
2025-05-30 23:55:30 +0530

    commit on sending and receiving message

---
+++ src/app/utils/models/Message.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose"
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    messageType: {
+      type: String,
+      enum: ["text", "image", "file"],
+      default: "text",
+    },
+
+    timestamp: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message;
