dee31e4350d87fd368ce0f7888c1a212d9214a38
KaranNegi0102 <karannegi0102@gmail.com>
2025-06-03 14:22:32 +0530

    testing render

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -7,7 +7,7 @@ import "dotenv/config";
 export const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: process.env.CLIENT_URL || "http://localhost:3000",
+      origin: process.env.CLIENT_URL || "http://localhost:3000" || "https://project0102.vercel.app",
       methods: ["GET", "POST"],
     },
   });
--- src/app/utils/dataBase/dbConnection.js
+++ src/app/utils/dataBase/dbConnection.js
@@ -1,13 +1,23 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-export default async function connectionDB (){
-  try{
-    await mongoose.connect("mongodb+srv://karan0102:Gautamn49@projects.yrofojl.mongodb.net/videoandtextWEB"
-    ).then(()=>{console.log("Connected to MongoDB");}
-    ).catch((error)=>{
-      console.log("mongoose connection error -> ",error);
-    })
-  }catch(error){
-    console.log("error in connecting to the database -> ",error);
+// Load environment variables
+dotenv.config();
+
+export default async function connectionDB() {
+  const dbUrl = process.env.DATABASE_URL;
+  console.log("this is dburl in mongodb",dbUrl);
+
+  if (!dbUrl) {
+    console.error("DATABASE_URL is not defined in environment variables");
+    throw new Error("DATABASE_URL is not defined in environment variables");
+  }
+
+  try {
+    await mongoose.connect(dbUrl);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    throw error;
   }
 }
